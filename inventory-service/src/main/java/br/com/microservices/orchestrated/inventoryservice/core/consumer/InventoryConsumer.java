b24d516ed36fc031a979cc5f6cb41292b9c48b9e
leo <nascimentoleo899@gmail.com>
2023-11-05 18:51:08 -0300

    config consumers

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/consumer/InventoryConsumer.java
@@ -0,0 +1,30 @@
+package br.com.microservices.orchestrated.inventoryservice.core.consumer;
+
+import br.com.microservices.orchestrated.inventoryservice.core.utils.JsonUtils;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class InventoryConsumer {
+    private final JsonUtils jsonUtils;
+
+    @KafkaListener(groupId = "${spring.kafka.consumer.group-id}",
+            topics =  "${spring.kafka.topic.inventory-success}")
+    public void consumerSuccessEvent(String payload){
+        log.info("Receiving success event {} from inventory-success topic", payload);
+        var event = jsonUtils.toEvent(payload);
+        log.info(event.toString());
+    }
+
+    @KafkaListener(groupId = "${spring.kafka.consumer.group-id}",
+            topics =  "${spring.kafka.topic.inventory-fail}")
+    public void consumerFailEvent(String payload){
+        log.info("Receiving rollback event {} from inventory-fail topic", payload);
+        var event = jsonUtils.toEvent(payload);
+        log.info(event.toString());
+    }
+}
