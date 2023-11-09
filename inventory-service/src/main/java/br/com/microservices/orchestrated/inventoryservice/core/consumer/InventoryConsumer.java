51cd05e9a00545c60e0f61f4083bfb5568e06c13
leo <nascimentoleo899@gmail.com>
2023-11-09 19:16:37 -0300

    implement inventory service

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/consumer/InventoryConsumer.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/consumer/InventoryConsumer.java
@@ -1,5 +1,6 @@
 package br.com.microservices.orchestrated.inventoryservice.core.consumer;
 
+import br.com.microservices.orchestrated.inventoryservice.core.service.InventoryService;
 import br.com.microservices.orchestrated.inventoryservice.core.utils.JsonUtils;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,13 +12,14 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class InventoryConsumer {
     private final JsonUtils jsonUtils;
+    private final InventoryService inventoryService;
 
     @KafkaListener(groupId = "${spring.kafka.consumer.group-id}",
             topics =  "${spring.kafka.topic.inventory-success}")
     public void consumerSuccessEvent(String payload){
         log.info("Receiving success event {} from inventory-success topic", payload);
         var event = jsonUtils.toEvent(payload);
-        log.info(event.toString());
+        inventoryService.updateInventory(event);
     }
 
     @KafkaListener(groupId = "${spring.kafka.consumer.group-id}",
@@ -25,6 +27,6 @@ public class InventoryConsumer {
     public void consumerFailEvent(String payload){
         log.info("Receiving rollback event {} from inventory-fail topic", payload);
         var event = jsonUtils.toEvent(payload);
-        log.info(event.toString());
+        inventoryService.rollBackInventory(event);
     }
 }
---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/model/OrderInventory.java
@@ -0,0 +1,51 @@
+package br.com.microservices.orchestrated.inventoryservice.core.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "order_invetory")
+public class OrderInventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "inventory_id", nullable = false)
+    private Inventory inventory;
+    @Column(nullable = false)
+    private String orderId;
+    @Column(nullable = false)
+    private String transactionId;
+    @Column(nullable = false)
+    private Integer orderQuantity;
+    @Column(nullable = false)
+    private Integer oldQuantity;
+    @Column(nullable = false)
+    private Integer newQuantity;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private LocalDateTime updateAt;
+
+    @PrePersist
+    public void prePersist(){
+        var now = LocalDateTime.now();
+        createdAt = now;
+        updateAt = now;
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        updateAt = LocalDateTime.now();
+    }
+}
